2279add28912ea8345fd77f612cfb4724f83abef
Leon Hsu <leonhsu95@gmail.com>
2021-04-01 13:43:17 +1100

    Tring to add array pushing

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -4,8 +4,6 @@ var currentDay = moment().format("dddd, Do MMMM");
 var currentDayEl = $('#current-day');
 currentDayEl.text("Today is " + currentDay);
 
-
-
 // Timeblock Elements
 var main = $("#main");
 var rowEl = $("<div>");
@@ -13,14 +11,13 @@ rowEl.attr("class","row time-block");
 main.append(rowEl);
 
 
-
 // Creating Timeblocks Dynamically
 for (let i = 0; i < 9; i++) {
-        var dayHour = moment().startOf("day").add(i+9,"h");
+        var hour = moment().startOf("day").add(i+9,"h");
 
         var hourEl = $("<div>");
         hourEl.attr("class","col-3 col-sm-2 col-lg-1 scheduler-hour hour");
-        hourEl.text(dayHour.format("hA"));
+        hourEl.text(hour.format("hA"));
         rowEl.append(hourEl);
         
         var schedulerPlannerEl = $("<div>");
@@ -28,17 +25,18 @@ for (let i = 0; i < 9; i++) {
         
         schedulerPlannerEl.attr("class","col-6 col-sm-8 col-lg-10 scheduler-planner");
         textAreaEl.attr({
+                            "id": "scheduler-input"+hour.format("H"),
                             "class":"scheduler-input", 
                             "cols":"5",
                             "rows":"3"
                         });
 
         // Changing Colour based on Hour Passed
-        if (moment(dayHour).isBefore(currentTime, "hour")){
+        if (moment(hour).isBefore(currentTime, "hour")){
             textAreaEl.addClass('past');
         }
 
-        else if(moment(dayHour).isSame(currentTime, "hour")){
+        else if(moment(hour).isSame(currentTime, "hour")){
             textAreaEl.addClass('present');
         }
 
@@ -51,13 +49,14 @@ for (let i = 0; i < 9; i++) {
         
         var saveEl= $("<div>");
         var saveFormEl = $("<form>");
+        saveFormEl.attr("form","event-form")
         var saveButtonEl = $("<button>");
         var saveIconEl =  $("<i>");
         
         saveEl.attr("class","col-6 col-sm-2 col-lg-1 save-button");
         saveFormEl.attr("class","save-form");
         saveButtonEl.attr({
-                            "class":"save", 
+                            "class":"save-button", 
                             "type": "submit"
                         });
         
@@ -67,5 +66,43 @@ for (let i = 0; i < 9; i++) {
         saveEl.append(saveFormEl);
         saveFormEl.append(saveButtonEl);
         saveButtonEl.append(saveIconEl);
-        
 }
+
+// Saving Text Area Content in Object Arrays
+var events = {
+    hours: [],
+    items: [],
+}
+
+
+function saveHour (hour) {
+    var hourItem = hour;
+    var index = events.hours.indexOf(hour);    
+    
+    if (hourItem !== ""){
+        // Adding new events in textarea
+        if (index === -1) {
+            events.hours.push(hour);
+            events.items.push(hourItem);
+        }
+        // No edits or change in textarea
+        else if (hourItem === events.items[index]) {
+            return null;
+        }
+       
+        // If no event item, no action
+        else {
+            return null;
+        }
+    }
+     localStorage.setItem("#scheduler-input", JSON.stringify(events));
+}
+
+
+
+// Save Button Click Event
+$(".save-button").on("click", function(event){
+    event.preventDefault();
+    event.stopPropagation();
+    saveHour(hour);
+})
